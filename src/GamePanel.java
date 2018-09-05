d0bbad762ee11a00baf9e484132bfbbc9401693f
ArnavPalkhiwala <league@iMac.attlocal.net>
2018-09-05 16:58:19 -0700

    new arrays

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -13,25 +13,26 @@ public class GamePanel extends JPanel implements KeyListener {
 	int imageRow = 3, imageColumn = 0;
 	int panelWidth = 700, panelHeight = 650;
 	public static BufferedImage rCat;
-	public static BufferedImage [][] bkgdArray = new BufferedImage [4] [1]; 
+	public static BufferedImage[][] bkgdArray = new BufferedImage[4][2];
 	RareCatto c;
 	ObjectManager objMana = new ObjectManager();
-	
-	//code for background
-	
+
+	// code for background
+
 	public GamePanel() throws IOException {
-		bkgdArray [0] [0] = ImageIO.read(this.getClass().getResourceAsStream("topframeww.png"));
-		bkgdArray [1] [0] = ImageIO.read(this.getClass().getResourceAsStream("frame3.png"));
-		bkgdArray [2] [0] = ImageIO.read(this.getClass().getResourceAsStream("pixil-frame-1.png"));
-		bkgdArray [3] [0] = ImageIO.read(this.getClass().getResourceAsStream("pixil-frame-0.png"));
+		bkgdArray[0][0] = ImageIO.read(this.getClass().getResourceAsStream("topframeww.png"));
+		bkgdArray[1][0] = ImageIO.read(this.getClass().getResourceAsStream("frame3.png"));
+		bkgdArray[2][0] = ImageIO.read(this.getClass().getResourceAsStream("pixil-frame-1.png"));
+		bkgdArray[3][0] = ImageIO.read(this.getClass().getResourceAsStream("pixil-frame-0.png"));
+		bkgdArray[0][1] = ImageIO.read(this.getClass().getResourceAsStream("topcenter.png"));
 		rCat = ImageIO.read(this.getClass().getResourceAsStream("CattoSprite (2).png"));
 		c = new RareCatto();
 		new Dimension(700, 700);
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
-		g.drawImage(bkgdArray [imageRow] [imageColumn], 0, 0, RunnerClass.bkgdwidth, RunnerClass.bkgdheight, null);
+		g.drawImage(bkgdArray[imageRow][imageColumn], 0, 0, RunnerClass.bkgdwidth, RunnerClass.bkgdheight, null);
 		c.draw(g);
 		repaint();
 	}
@@ -39,85 +40,80 @@ public class GamePanel extends JPanel implements KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 		if (e.getKeyCode() == KeyEvent.VK_A) {
-			
+
 			c.catX -= 10;
-			
+
 			if (imageRow == 2 && imageColumn == 0 && c.catY > 350 && c.catY < 395) {
 				c.catX -= 10;
 			}
-			
-			
-			
-			if (imageColumn == 0 && c.catX < 25) {
+
+			else if (imageColumn == 0 && c.catX < 25) {
 				c.catX += 10;
-				
+
 			}
 			
+			else if (imageColumn != 0 && c.catX < 0) {
+				imageColumn--;
+				c.catX = 675;
+			}
+
 		}
-		
+
 		else if (e.getKeyCode() == KeyEvent.VK_S) {
 			c.catY += 10;
-			
-			//if (imageRow == 2 && imageColumn == 0 && c.catX > 0 && c.catX < 25  && c.catY > 400) {
-			//	c.catY = 400;
-			//}
-			
-			if (c.catY >= panelHeight && imageRow != 2) {
+
+			// if (imageRow == 2 && imageColumn == 0 && c.catX > 0 && c.catX < 25 && c.catY
+			// > 400) {
+			// c.catY = 400;
+			// }
+
+			if (c.catY >= panelHeight && imageRow != 0) {
 				imageRow++;
 				c.catY = -12;
 			}
-			
+
 			else if (imageRow == 3 && c.catY >= 575) {
 				c.catY = 575;
 			}
-			
+
 		}
-		
+
 		else if (e.getKeyCode() == KeyEvent.VK_D) {
 			c.catX += 10;
+			
+			if (imageColumn != 3 && c.catX >= 675) {
+				imageColumn++;
+				c.catX = -10;
+			}
 		}
-		
-		else if (e.getKeyCode() == KeyEvent.VK_W && e.isShiftDown() == true) {
-			c.catY -= 20;
-			
-			if (c.catY < -16) {
-				imageRow--;
-				c.catY = panelHeight;
-			}
-			
-			if (imageRow == 0 && c.catY <= 575) {
-				c.catY = 575;
-			}
-	}
-		
+
 		else if (e.getKeyCode() == KeyEvent.VK_W) {
 			c.catY -= 10;
-			
+
 			if (c.catY < -16) {
 				imageRow--;
 				c.catY = panelHeight;
 			}
-			
+
 			if (imageRow == 0 && c.catY <= 25) {
 				c.catY = 25;
 			}
 		}
-			
-			
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
